e0eee402db4e82d279a2367bbb6ec60cc450bb4a
parkgunwook <pkw85428@naver.com>
2023-03-27 23:04:34 +0900

    1.컨í테스트코드 작성

---
+++ src/test/java/com/studybook/springboot/web/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.studybook.springboot.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void hello가_리턴된다() throws Exception {
+        //given
+        String hello = "hello";
+
+        //when
+        mvc.perform(get("/hello"))
+                //then
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
