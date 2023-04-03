88241a25fa11cfbb5b46aa51849145436ccd265f
parkgunwook <pkw85428@naver.com>
2023-04-03 23:01:36 +0900

    2.two commit

--- src/test/java/com/studybook/springboot/web/HelloControllerTest.java
+++ src/test/java/com/studybook/springboot/web/HelloControllerTest.java
@@ -27,4 +27,17 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
+    @Test
+    void helloDTO가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name",name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
 }
